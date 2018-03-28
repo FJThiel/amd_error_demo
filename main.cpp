ccd7e194ffaa51c3898b532cbc0b83a677a48aaf
Felix Thiel <felix.thiel@student.hpi.de>
2018-03-28 16:34:27 +0200

    Added workaroud (it will work if the OpenGL Compat Profile is used)

--- main.cpp
+++ main.cpp
@@ -187,7 +187,8 @@ int main(int /*argc*/, char * /*argv[]*/) {
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // will not work
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE); // works
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
 
